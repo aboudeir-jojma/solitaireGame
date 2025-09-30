cbec2cb4b493b05e8f80a964508942302fd1650b
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-09-30 12:05:07 +0100

    sitamap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://247solitairegames.com', // remplace par ton domaine
+  generateRobotsTxt: true, // génère aussi robots.txt
+  sitemapSize: 7000,
+};
